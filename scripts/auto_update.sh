a93f5f7018fc19d5bb745e24a16c57d32567e87a
Siyuan Fu <fusiyuan2010@gmail.com>
2014-07-01 04:52:20 +0000

    Improved Make file, movie execs into another dir. Improved automatic scripts

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-echo "./auto_update.sh filelist.txt outputdir db_build.exec output.file"
-echo "example: ./auto_update.sh filelist.txt /disk2/imdb/ ~/IMDB_predictor/src/db_build temp.dat"
+#/etc/crontab run 3am every day
+#movie_predictor auto data download and server restart
+#0 3     * * *   ubuntu  date >> /home/ubuntu/update.log; /home/ubuntu/IMDB_predictor/scripts/auto_update.sh /home/ubuntu/IMDB_predictor/ /data/imdb/ >> /home/ubuntu/imdb_update.log
+
+echo "./auto_update.sh imdb_working_path data_path"
+echo "example: ./auto_update.sh ~/IMDB_predictor/ /disk2/imdb"
 newdata=0
 
-for i in `cat $1`
+cd $1
+
+for i in `cat ./scripts/filelist.txt`
 do
     curl  -I "ftp://ftp.funet.fi/pub/mirrors/ftp.imdb.com/pub/${i}.list.gz" > tmp
     diff tmp $2/${i}.info
@@ -23,7 +29,19 @@ rm -f tmp
 
 if [ $newdata -eq 1 ]
 then
-#Run the parsers
-$3 $4 $2
+	#Run the parsers
+	#prevent insufficient memory, kill current running instance first
+
+	echo "New Data found, have to kill running server"
+	sudo kill `ps aux|grep imdb_server | grep -v -E "grep|nohup" | awk '{print $2}'`
+
+	echo "killed, begin to build"
+	./build/imdb_db_build imdb_predictor.dat $2
+
+	echo "data built"
+	sudo nohup ./build/imdb_server 80 $2/imdb_predictor.dat ./html/ &
+
+	echo "server started again"
 fi
+
 
